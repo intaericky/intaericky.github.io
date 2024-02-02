2be60f1836f918a84db8c2a597ead7d2fdbb5b87
Intae Hwang <intaericky@gmail.com>
2024-02-02 21:28:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function init() {
 function draw() {
     updateCanvasSize();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.save();
     window.requestAnimationFrame(draw);
